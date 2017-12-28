0955bb6ec79e4dfd9797c96af424af118ba52242
bellavin.anton <bellavin.anton@itrium.ru>
2017-12-28 21:06:54 +0300

    Initial project

---
+++ laba2/src/ControllerServlet.java
@@ -0,0 +1,32 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name = "ControllerServlet",
+        urlPatterns = "/ControllerServlet"
+)
+public class ControllerServlet extends HttpServlet {
+    private String message;
+
+    public void init() throws ServletException {
+        message = "Hello world!";
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+}
---
+++ laba2/web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: anton
+  Date: 19.12.17
+  Time: 23:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+    <link href="../css/main.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <h1>Hello</h1>
+  </body>
+</html>
