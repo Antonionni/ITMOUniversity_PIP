259d014b1c1c6156ee1b67bc24e3f35b90212e9a
bellavin.anton <bellavin.anton@itrium.ru>
2018-01-01 18:04:02 +0300

    change web components

--- laba2/src/main/webapp/resources/js/main.js
+++ laba2/src/main/webapp/resources/js/main.js
@@ -1,20 +1,14 @@
 (function() {
-    function setPixel(x, y) {
-        var point = document.createElement('div');
-        point.className = "point";
-        point.style.left = x+"px";
-        point.style.top = y+"px";
-        document.getElementById("plan").appendChild(point);
-    }
+    var isMinus = false;
     function handleCheckInput(event) {
-        if (event.target.value === '-') {
+        if (event.target.value == "-") {
             return;
         }
-        var value = parseInt(event.target.value) || 0;
+        var value = parseInt(this.value) || 0;
         if (!value) {
             console.log("value is not number");
             this.value = "";
-        } else if (value > 5 && value < -5) {
+        } else if (value > 3 || value < -5) {
                 this.value = "";
         }
     }
@@ -22,12 +16,18 @@
         var imagePointer = document.getElementById("area-image");
         var imageW = imagePointer.width;
         var imageH = imagePointer.height;
-
         var pageX = event.layerX;
         var pageY = event.layerY;
-        setPixel(pageX, pageY);
 
-        console.log("x = ", event.offsetX, " y = ", event.offsetY, " imageH", imageH, " imageW", imageW);
+        _setPixel(pageX, pageY, false);
+    }
+    function _setPixel(x, y, isHitting) {
+        var point = document.createElement('div');
+        point.className = "point";
+        point.style.left = x+"px";
+        point.style.top = y+"px";
+        point.style.backgroundColor = isHitting ? "green" : "red";
+        document.getElementById("plan").appendChild(point);
     }
     document.getElementById("y-cord-input").addEventListener('input', handleCheckInput, false);
     document.getElementById("area-image").addEventListener('click', handleClick, false);
